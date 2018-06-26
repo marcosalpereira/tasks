f5c36ba3e2bb341c6fe7e79b4526bea3d1bbfafd
marcosalpereira <marcos.lacerda@serpro.gov.br>
2018-06-26 07:51:17 -0300

    disable mouse meny

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,7 +39,7 @@ export class AppComponent implements OnInit {
       angle: 30,
 
       // The mouse distance from menu position which can trigger menu to open.
-      threshold: 40,
+      threshold: 0,
 
       // Width(in px) of the thin line you see on screen when menu is in closed position.
       overlap: 6,
