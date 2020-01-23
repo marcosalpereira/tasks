bbca055999de03d6dc3b3c7b55b7e0f119494eb7
marcosalpereira <marcosalpereira@gmail.com>
2020-01-23 16:48:30 -0300

    wip: auto-update

--- src/app/electron.service.ts
+++ src/app/electron.service.ts
@@ -1,4 +1,4 @@
-import { remote } from 'electron';
+import { app, remote } from 'electron';
 import * as childProcess from 'child_process';
 import * as fs from 'fs';
 import * as path from 'path';
@@ -10,19 +10,28 @@ export class ElectronService {
   path: typeof path;
   remote: typeof remote;
   os: typeof os;
+  app: typeof app;
 
   constructor() {
     if (this.isElectron()) {
       this.childProcess = window.require('child_process');
       this.fs = window.require('fs');
       this.path = window.require('path');
-      this.remote = window.require('electron').remote;
       this.os = window.require('os');
+
+      const electron = window.require('electron');
+      this.remote = electron.remote;
+      this.app = this.remote.app;
     }
   }
 
   isElectron = () => {
     return window && window.process && window.process.type;
+  }
+
+  reload() {
+    this.app.relaunch();
+    this.app.exit(0);
   }
 
 }
